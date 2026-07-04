d9e0ce007632ad87ab67aff5d40ff36a0c49c847
Rene Sebera <renesebera@192.168.1.10>
2026-07-04 09:52:38 +0530

    Add actual Java test code to hjm.java

--- hjm.java
+++ hjm.java
@@ -2,10 +2,13 @@ public class hjm {
     public static void main(String[] args) {
         String message = "Hello, World!";
         int Roll = 45;
+        int income = 20;
         String character = "Man";
         System.out.println(message);
         System.out.println("Roll Number: " + Roll);
         System.out.println("Character: " + character);
-    }
+        System.out.println("Income: " + income);
+
+    :
 
 }
