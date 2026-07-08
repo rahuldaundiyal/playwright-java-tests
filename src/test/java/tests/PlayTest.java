e3ac289758abf356146ad5b7c7ac7c3dc86d9bee
Rene Sebera <renesebera@192.168.1.4>
2026-07-08 11:27:15 +0530

    Fix project compilation, packages, and keep PlayTest working

--- src/test/java/tests/PlayTest.java
+++ src/test/java/tests/PlayTest.java
@@ -1,28 +1,21 @@
-package src.test.java;
+package tests;
+
 import com.microsoft.playwright.*;
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class PlayTest {
 
     @Test
     void verifyExampleDotComTitle() {
-
         try (Playwright playwright = Playwright.create()) {
-
             Browser browser = playwright.chromium().launch(
                     new BrowserType.LaunchOptions().setHeadless(true));
 
             Page page = browser.newPage();
-
-            // Navigate to the website
             page.navigate("https://example.com");
 
-            // Get the page title
             String title = page.title();
-
-            // Verify the title
             assertTrue(title.contains("Example Domain"));
 
             System.out.println("Page Title: " + title);
